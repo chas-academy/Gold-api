56b5750e782630f439c7f54d82dba2c24c2bd3b6
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-04-12 16:07:20 +0200

    update files

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,22 @@
 if (!process.env.PG_DB) {
-  const fs = require('fs')
-  const dotenv = require('dotenv')
-  const envConfig = dotenv.parse(fs.readFileSync('.env'))
+    const fs = require("fs");
+    const dotenv = require("dotenv");
+    const envConfig = dotenv.parse(fs.readFileSync(".env"));
 
-  for (var k in envConfig) {
-    process.env[k] = envConfig[k]
-  }
+    for (var k in envConfig) {
+        process.env[k] = envConfig[k];
+    }
 
-  console.log('[api][sequelize] Loaded database ENV vars from .env file')
+    console.log("[api][sequelize] Loaded database ENV vars from .env file");
 }
 
 module.exports = {
-  development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-    migrationStorageTableName: 'sequelize_meta'
-  }
-}
+    development: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        host: process.env.POSTGRES_HOST,
+        dialect: "postgres",
+        migrationStorageTableName: "sequelize_meta"
+    }
+};

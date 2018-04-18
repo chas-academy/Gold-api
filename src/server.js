3eafcd5661291aae8855b17c88002b0f6462db5f
zavham <31238983+zavham@users.noreply.github.com>
2018-04-18 16:50:32 +0200

    basic express server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
+var express = require('express')
+var bodyParser = require('body-parser')
 
+const app = express()
+const port = process.env.PORT || 7770
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res, next) {
+    res.send('GOLD Server Running on http://localhost:' + port + ' or for windows users 192.168.99.100:' + port)
+})
+
+app.listen(port, () => {
+    console.log('[api][listen] http://localhost:' + port);
+})

637b2440bfca420c2506f047b5878ecd088e4cb4
zavham <31238983+zavham@users.noreply.github.com>
2018-04-26 16:29:20 +0200

    customers seeds

--- src/seeders/20180420133645-customers.js
+++ src/seeders/20180420133645-customers.js
@@ -2,19 +2,31 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // return queryInterface.bulkInsert('customers', [{
-    //   user_id: '3',
-    //   type: 'private',
-    //   email: 'sven.svensson@gmail.com',
-    //   tel: '0737223452',
-    //   address: 'Drottninggatan 14, 2tr'
-    // }, {
-    //   user_id: '4',
-    //   type: 'company',
-    //   email: 'info@maklarringen.com',
-    //   tel: '08246812',
-    //   address: 'Grinstagatan 49'
-    // }], {});
+    return queryInterface.bulkInsert('customers', [{
+      user_id: '5',
+      type: 'private',
+      email: 'sven.svensson@gmail.com',
+      tel: '0737223452',
+      address: 'Drottninggatan 14, 2tr'
+    }, {
+      user_id: '6',
+      type: 'private',
+      email: 'bob@gmail.com',
+      tel: '073098763',
+      address: 'Grinstagatan 49'
+    }, {
+      user_id: '7',
+      type: 'company',
+      email: 'info@maklarringen.com',
+      tel: '08246812',
+      address: 'Elektravägen 29'
+    }, {
+      user_id: '8',
+      type: 'company',
+      email: 'company@name.com',
+      tel: '08246843',
+      address: 'Solursgränd 2'
+    }], {});
   },
 
   down: (queryInterface, Sequelize) => {

76cd1fb45270e95a549ad9a60ea9f4ba5f15250a
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-04-26 16:40:26 +0200

    hotfix in users seeds

--- src/seeders/20180420131734-users.js
+++ src/seeders/20180420131734-users.js
@@ -11,43 +11,43 @@ module.exports = {
         id: 2,
         type: 'employee',
         name: 'Erica',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019877',
         password: 'qwerty'
       }, {
         id: 3,
         type: 'employee',
         name: 'Sven',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019878',
         password: 'qwerty'
       }, {
         id: 4,
         type: 'employee',
         name: 'Abdi',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019879',
         password: 'qwerty'
       }, {
         id: 5,
         type: 'customer',
         name: 'Sven Svensson',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019870',
         password: 'qwerty'
       }, {
         id: 6,
         type: 'customer',
         name: 'Bob Johnson',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019875',
         password: 'qwerty'
       }, {
         id: 7,
         type: 'customer',
         name: 'Mäklarringen',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019874',
         password: 'qwerty'
       }, {
         id: 8,
         type: 'customer',
         name: 'Hidden Company',
-        pers_org_num: '199901019876',
+        pers_org_num: '199901019873',
         password: 'qwerty'
       }], {});
   },

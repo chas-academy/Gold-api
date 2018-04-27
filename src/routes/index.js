04c542e1ef85b76968103e98c90dc4f9c69448d7
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-04-27 14:56:57 +0200

    basic routes before branching out

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import C from '../controllers';
 
 export default app => {
-    app.get('/users', C.Services.index)
+    app.get('/services', C.Services.index)
+    app.get('/services/:id', C.Services.show)
+    app.get('/users', C.Users.index)
+    app.get('/users/:id', C.Users.show)
 }

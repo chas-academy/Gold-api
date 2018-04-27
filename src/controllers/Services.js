4b8a4e9df133803803e427f32b57739bbc8e03b2
zavham <31238983+zavham@users.noreply.github.com>
2018-04-27 16:44:42 +0200

    basic crud operations in Service controller

--- src/controllers/Services.js
+++ src/controllers/Services.js
@@ -2,11 +2,61 @@ var models = require('../models');
 var Service = require('../models').service;
 
 module.exports = {
-
+    //Get a list of services using model.findAll()
     index(req, res) {
         Service.findAll({ include: [{ model: models.internal_order, as: "int_orders" },
-         { model: models.order }, { model: models.complaint, as: "complaints" }] }).then(function (services) {
-            res.send(services);
+         { model: models.order }, { model: models.complaint, as: "complaints" }] })
+         .then(function (services) {
+            res.status(200).json(services);
         })
+        .catch(function (error) {
+            res.status(500).json(error);
+        });
+    },
+    //Get a service by the unique id using model.findById()
+    show(req, res) {
+        Service.findById(req.params.id, {
+            include: [{ model: models.internal_order, as: "int_orders" },
+        { model: models.order }, { model: models.complaint, as: "complaints" }] })
+        .then(function (services) {
+            res.status(200).json(services);
+        })
+        .catch(function (error) {
+            res.status(500).json(error);
+        });
+    },
+    //Create a new service using model.create()
+    create(req, res) {
+        Service.create({
+            order_type: req.body.type,
+            con_pers: req.body.con_pers,
+            con_tel: req.body.con_tel
+            // status: req.body.status
+        })
+        .then(function (newService) {
+            res.status(200).json(newService);
+        })
+        .catch(function (error) {
+            res.status(500).json(error);
+        });
+    },
+    //Edit existing service details using model.update()
+    update(req, res) {
+        Service.findById(req.params.id).then(function (Service) {
+            Service.update({
+                order_type: req.body.type,
+                con_pers: req.body.con_pers,
+                con_tel: req.body.con_tel
+            })
+            .then(function (updateServices) {
+                res.status(200).json(updateServices);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            });
+        })
+        .catch(function (error) {
+            res.status(500).json(error);
+        });
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import C from '../controllers';
 export default app => {
     app.get('/services', C.Services.index)
     app.get('/services/:id', C.Services.show)
-    app.get('/users', C.Users.index)
-    app.get('/users/:id', C.Users.show)
+    app.post('/services/create', C.Services.create)
+    app.put('/services/update/:id', C.Services.update)
+    // app.get('/users', C.Users.index)
+    // app.get('/users/:id', C.Users.show)
 }

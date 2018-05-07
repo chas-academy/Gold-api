0a351902ea1f184c6ac455ef7d0a527a0a23f912
zavham <31238983+zavham@users.noreply.github.com>
2018-05-07 15:14:25 +0200

    add internal orders and complaints controllers

--- src/controllers/Complaints.js
+++ src/controllers/Complaints.js
@@ -1,6 +1,91 @@
 var models = require('../models');
 var Complaint = require('../models').complaint;
+var Service = require('../models').service;
 
 module.exports = {
 
+    //Get a list of complaints
+    index(req, res) {
+        Complaint.findAll({
+            include: [{ model: models.service }]
+        })
+            .then(function (complaint) {
+                res.status(200).json(complaint);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            });
+    },
+    //Get a list of complaints by specific id
+    show(req, res) {
+        Complaint.findById(req.params.id, {
+            include: [
+                { model: models.service }]
+        })
+            .then(function (complaint) {
+                res.status(200).json(complaint);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            });
+    },
+    //Create complaints
+    create(req, res) {
+        // let hours = req.body.time.split(":")[0]
+        // if (hours < 10 && hours.length < 2) {
+        //     req.body.time = "0" + req.body.time
+        // }
+        Service.create({
+            order_type: "complaint",
+            order_id: req.body.order_id,
+            // datetime: new Date(req.body.date + "T" + req.body.time),
+            complaint: {
+                description: req.body.description,
+                image_path: req.body.image_path
+            }
+        }, {
+                include: [models.complaint]
+            })
+            .then(function (complaint) {
+                res.status(200).json(complaint);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            });
+    },
+    //Update complaints
+    update(req, res) {
+        // let hours = req.body.time.split(":")[0]
+        // if (hours < 10 && hours.length < 2) {
+        //     req.body.time = "0" + req.body.time
+        // }
+        Service.findById(req.params.id, { include: [models.complaint] }).then(function (Service) {
+            Service.update({
+                order_type: "complaint",
+                datetime: new Date(req.body.date + "T" + req.body.time),
+                complaint: {
+                    description: req.body.description,
+                    image_path: req.body.image_path
+                }
+
+            }, {
+                    include: [models.complaint]
+                })
+        })
+            .then(function (complaint) {
+                res.status(200).json(complaint);
+            })
+            .catch(function (error) {
+                res.status(500).json(error);
+            })
+    },
+    //Delete complaints by id
+    destroy(req, res) {
+        Service.destroy({
+            where: {
+                id: req.params.id
+            },
+
+        })
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import C from '../controllers';
 
 export default app => {
-  
+
     //<--auth routes-->
     app.post('/register', C.Auth.register)
     app.post('/login', C.Auth.login)
-  
+
     //<--users routes-->
     app.get('/users', C.Auth.loginRequired, C.Users.all)
     app.get('/users/:id', C.Auth.loginRequired, C.Users.find)
@@ -15,23 +15,32 @@ export default app => {
     app.put('/users/update/:id', C.Auth.loginRequired, C.Users.update)
     app.put('/customers/update/:id', C.Auth.loginRequired, C.Users.updateCustomer)
     app.delete('/users/delete/:id', C.Auth.loginRequired, C.Users.destroy)
-  
+
     //<--services routes-->
     app.get('/services', C.Auth.loginRequired, C.Services.index)
     app.get('/services/new', C.Auth.loginRequired, C.Services.showNew)
     app.get('/services/taken', C.Auth.loginRequired, C.Services.showTaken)
     app.get('/services/done', C.Auth.loginRequired, C.Services.showDone)
-  
+
     //<--orders routes-->
     app.get('/orders', C.Auth.loginRequired, C.Orders.index)
     app.get('/orders/:id', C.Auth.loginRequired, C.Orders.show)
     app.post('/orders/create', C.Auth.loginRequired, C.Orders.create)
     app.put('/orders/update/:id', C.Auth.loginRequired, C.Orders.update)
     app.delete('/orders/delete/:id', C.Auth.loginRequired, C.Orders.destroy)
-  
-    //<--internal orders routes-->
-  
-    //<--complaints-->
 
-    
+    //<--internal orders routes-->
+    app.get('/int_orders', C.IntOrders.index)
+    app.get('/int_orders/:id', C.IntOrders.show)
+    app.post('/int_orders/create', C.IntOrders.create)
+    app.put('/int_orders/update/:id', C.IntOrders.update)
+    app.delete('/int_orders/delete/:id', C.IntOrders.destroy)
+
+    //<--complaints-->
+    app.get('/complaints', C.Complaints.index)
+    app.get('/complaints/:id', C.Complaints.show)
+    app.post('/complaints/create', C.Complaints.create)
+    app.put('/complaints/update/:id', C.Complaints.update)
+    app.delete('/complaints/delete/:id', C.Complaints.destroy)
+
 }

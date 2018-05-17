1e9c986be827542bb9db02fbd27e890c19a290de
Oleg-Lopes <oleg.lopes@chasacademy.se>
2018-05-17 15:15:00 +0200

    lat lon seeders change to int

--- src/seeders/20180420133645-customers.js
+++ src/seeders/20180420133645-customers.js
@@ -6,26 +6,26 @@ module.exports = {
       user_id: '5',
       type: 'private',
       address: 'Drottninggatan 14',
-      lat: '59.3298085',
-      lon: '18.0633878'
+      lat: 59.3298085,
+      lon: 18.0633878
     }, {
       user_id: '6',
       type: 'private',
       address: 'Grinstagatan 49',
-      lat: '59.3583324',
-      lon: '17.8692312'
+      lat: 59.3583324,
+      lon: 17.8692312
     }, {
       user_id: '7',
       type: 'company',
       address: 'Elektravägen 29',
-      lat: '59.2950814',
-      lon: '18.0088671'
+      lat: 59.2950814,
+      lon: 18.0088671
     }, {
       user_id: '8',
       type: 'company',
       address: 'Solursgränd 2',
-      lat: '59.3627103',
-      lon: '17.8738748'
+      lat: 59.3627103,
+      lon: 17.8738748
     }], {});
   },
 
--- src/seeders/20180427090323-internal_orders.js
+++ src/seeders/20180427090323-internal_orders.js
@@ -5,15 +5,15 @@ module.exports = {
       return queryInterface.bulkInsert('internal_orders', [{
         service_id: 11,
         address: 'Konstigt gatan 12',
-        lat: '60.3298085',
-        lon: '15.0633878',
+        lat: 60.3298085,
+        lon: 15.0633878,
         description: 'Buy plz some of those and more of these.',
         image_path: null
       }, {
         service_id: 12,
         address: 'Konstigt gatan 12',
-        lat: '58.3298085',
-        lon: '17.0633878',
+        lat: 58.3298085,
+        lon: 17.0633878,
         description: 'Buy plz some of those and more of these.',
         image_path: null
       }], {});
